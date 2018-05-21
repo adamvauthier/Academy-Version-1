5fb2b1b5969841423b41e83c4b1044c69ae9f9b7
Adam Vauthier <adamvauthier@Adams-MacBook-Pro.local>
2018-05-21 12:20:26 -0600

    GDPR

--- wp-content/themes/academies_v1/header.php
+++ wp-content/themes/academies_v1/header.php
@@ -185,6 +185,10 @@ function createDateRangeArray($strDateFrom,$strDateTo)
 <body <?php body_class(); ?>>
 <div id="page" class="hfeed site">
 	<a class="skip-link screen-reader-text" href="#content"><?php esc_html_e( 'Skip to content', 'academies_v1' ); ?></a>
+	<div class="gdpr-popup">
+			This site uses cookies to store information on your computer. By using this site, you consent to the placement and use of these cookies. Read our <a href="/privacy-policy/">Privacy Policy</a> to learn more.  
+			<a href="#" class="accept">ACCEPT </a> 
+		</div>
 	<header id="masthead" class="site-header" role="banner">
     <section class="container">
       <a href="http://dioceseofbrooklyn.org/" target="_blank"><div class="home-icon"></div></a>
